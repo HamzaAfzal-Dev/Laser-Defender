9502bc2a2f374550c0cdee27bcd38ea12a906e1b
Hamza Afzal <95589034+ArceusDev@users.noreply.github.com>
2023-01-25 21:34:15 +0500

    Added Player Movement
    Added player movement in Player.cs file. Player movement is framerate independent.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+
+    float xMin, xMax, yMin , yMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        float newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
