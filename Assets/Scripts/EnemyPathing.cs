86b9ff3ec25e5ba34f16bef2ef5c3aee0c27e8ed
Hamza Afzal <95589034+ArceusDev@users.noreply.github.com>
2023-01-26 01:24:02 +0500

    Config File Made

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] WaveConfig waveConfig;
+    [SerializeField] float moveSpeed = 2f;
+
+    List<Transform> waypoints;
+    int waypointIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            Vector3 targetPosition = waypoints[waypointIndex].transform.position;
+            float movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
